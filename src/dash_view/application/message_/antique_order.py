d491cfad20f398b7d5f95b146ec0a624d6f966b6
Bcccccc03 <138638778+Bcccccc03@users.noreply.github.com>
2025-05-21 23:14:52 +0800

    Add files via upload

--- src/dash_view/application/message_/antique_order.py
+++ src/dash_view/application/message_/antique_order.py
@@ -7,6 +7,8 @@ from dash_components import Card
 import dash_callback.application.message_.antique_order_c  # noqa: F401
 from i18n import t__notification, translator
 
+from src.database.sql_db.dao.dao_antique_order import Antique
+from src.database.sql_db.entity.table_antique_order import SysAntique
 
 # 二级菜单的标题、图标和显示顺序
 title = '文物管理'
@@ -15,8 +17,9 @@ logger = Log.get_logger(__name__)
 order = 2
 access_metas = ('文物管理-页面',)
 
-
 def render_content(menu_access: MenuAccess, **kwargs):
+    query=SysAntique.select(SysAntique.Classifications)
+    print(query)
     return [
         fac.Fragment(
             [
@@ -30,34 +33,18 @@ def render_content(menu_access: MenuAccess, **kwargs):
                         fac.AntdSelect(
                             id='antique-status-filter',
                             options=[
-                                {'label': t__notification('待审核'), 'value': -1},
-                                {'label': t__notification('已驳回'), 'value': 0},
-                                {'label': t__notification('已通过'), 'value': 1},
+                                {'label': t__notification('绘画'), 'value':'绘画'},
+                                {'label': t__notification('纺织品'), 'value': '纺织品'},
+                                {'label': t__notification('装饰艺术'), 'value': '装饰艺术'},
+                                {'label': t__notification('雕塑'), 'value': '雕塑'},
+                                {'label': t__notification('金属艺术'), 'value': '金属艺术'},
+                                {'label': t__notification('玉石与石头'), 'value': '玉石与石头'},
+                                {'label': t__notification('印刷品和图纸'), 'value': '印刷品和图纸'},
+                                {'label': t__notification('书籍和手稿'), 'value': '书籍和手稿'}
                             ],
                             mode='multiple',
-                            placeholder=t__notification('请选择文物状态'),
+                            placeholder=t__notification('请选择文物种类'),
                             style={'width': '200px'},
-                        ),
-                        fac.AntdPopconfirm(
-                            fac.AntdButton(
-                                t__notification('通过选中'),
-                                type='primary',
-                                icon=fac.AntdIcon(icon='antd-check'),
-                            ),
-                            id='antique-button-pass',
-                            title=t__notification('确认通过选中行吗？'),
-                            locale=translator.get_current_locale(),
-                        ),
-                        fac.AntdPopconfirm(
-                            fac.AntdButton(
-                                t__notification('驳回选中'),
-                                type='primary',
-                                danger=True,
-                                icon=fac.AntdIcon(icon='antd-close'),
-                            ),
-                            id='antique-button-reject',
-                            title=t__notification('确认驳回选中行吗？'),
-                            locale=translator.get_current_locale(),
                         ),
                     ]
                 ),
