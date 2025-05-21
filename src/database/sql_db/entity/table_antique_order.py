a0f36caae599838a3cb8dd45a9780763b5c37c56
Bcccccc03 <138638778+Bcccccc03@users.noreply.github.com>
2025-05-21 23:16:08 +0800

    Add files via upload

--- src/database/sql_db/entity/table_antique_order.py
+++ src/database/sql_db/entity/table_antique_order.py
@@ -8,12 +8,17 @@ class BaseModel(Model):
 
 
 class SysAntique(BaseModel):
-    relic_id = CharField(primary_key=True)
-    relic_type = IntegerField()
-    relic_name = CharField()
-    relic_time = CharField()
-    relic_loc = CharField()
-    relic_intro = TextField()
+    id = CharField(primary_key=True)
+    Classifications = CharField()
+    Artist=CharField()
+    Credit= CharField()
+    Description= CharField()
+    Materials= CharField()
+    Dimensions = TextField()
+    Dynasty=CharField()
+    Title=CharField()
+    ImgUrl=CharField()
+    ImgPath=CharField()
     spare_id = IntegerField(default=-1)  # -1: 待审核, 0: 已驳回, 1: 已通过
     class Meta:
-        table_name = 'antiques'
+        table_name = 'art'
